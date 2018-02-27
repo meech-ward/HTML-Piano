778b94e1dcc34169f402871b9de310ad5f4c3c7f
Sam Meech-Ward <sam@meech-ward.com>
2018-02-26 18:15:03 -0800

    Refactor the code a little better

--- piano/src/piano.js
+++ piano/src/piano.js
@@ -9,56 +9,62 @@ All piano keys are indexed from 1 to whatever. Not 0
 let pianoObj = {
   keyUp: () => {},
   keyDown: () => {},
-};
-
-function newPiano(whiteKeysAmount, blackKeysLayout) {
-  const piano = Object.create(pianoObj);
-  piano.HTML = pianoDOM.pianoElementMake.call(pianoObj);
-
-  piano.blackKeysArray = pianoUtilities.arrayFromBlackKeys(blackKeysLayout);
-
-  const blackKeyWidthPercent = 100.0/whiteKeysAmount/1.5;
-  const whiteKeyWidthPercent = 100.0/whiteKeysAmount;
-
-  (function() {
-    for (let i = 1; i <= whiteKeysAmount; i++) {
-      const pianoKeyNumber = pianoUtilities.pianoKeyNumberFromWhiteKeyNumber(i, this.blackKeysArray);
-      pianoDOM.whiteKeyMake.call(this, i, whiteKeyWidthPercent, pianoKeyNumber, whiteKeysAmount);
-    }
-  }).call(piano);
-
-  (function() {
-    let blackKeyNumber = 1;
-    
-    let numberOfWhitesSinceLastBlack = 0;
-    let isFirst = true;
-    for (let i = 0; i <= this.blackKeysArray.length-1; i++) {
-      if (this.blackKeysArray[i] === 0) {
-        numberOfWhitesSinceLastBlack++;
-        continue;
-      }
-      pianoDOM.blackBlankMake.call(this, numberOfWhitesSinceLastBlack, blackKeyWidthPercent, whiteKeyWidthPercent, isFirst);
-      const numberOfWhites = i;
-      pianoDOM.blackKeyMake.call(this, blackKeyNumber, blackKeyWidthPercent, numberOfWhites+blackKeyNumber, whiteKeysAmount);
-      blackKeyNumber++;
-      numberOfWhitesSinceLastBlack = 0;
-      isFirst = false;
-    }
-  }).call(piano);
-
-  (function() {
-  this._keyDown = (key) => {
+  _keyDown: function(key) {
     key.classList.remove(pianoClassNames.keyUp);
     key.classList.add(pianoClassNames.keyDown);
     this.keyDown(pianoUtilities.keyNumber(key));
-  }
-  this._keyUp = (key) => {
+  },
+  _keyUp: function(key) {
     key.classList.add(pianoClassNames.keyUp);
     key.classList.remove(pianoClassNames.keyDown);
     this.keyUp(pianoUtilities.keyNumber(key));
   }
-}).call(piano);
+};
 
+function buildWhiteKeys() {
+  for (let i = 1; i <= this.whiteKeysAmount; i++) {
+    const pianoKeyNumber = pianoUtilities.pianoKeyNumberFromWhiteKeyNumber(i, this.blackKeysArray);
+    pianoDOM.whiteKeyMake.call(this, i, this.whiteKeyWidthPercent, pianoKeyNumber, this.whiteKeysAmount);
+  }
+}
+
+function buildBlackKeys() {
+  let blackKeyNumber = 1;
+    
+  let numberOfWhitesSinceLastBlack = 0;
+  let isFirst = true;
+  for (let i = 0; i <= this.blackKeysArray.length-1; i++) {
+    if (this.blackKeysArray[i] === 0) {
+      numberOfWhitesSinceLastBlack++;
+      continue;
+    }
+    pianoDOM.blackBlankMake.call(this, numberOfWhitesSinceLastBlack, this.blackKeyWidthPercent, this.whiteKeyWidthPercent, isFirst);
+    const numberOfWhites = i;
+    pianoDOM.blackKeyMake.call(this, blackKeyNumber, this.blackKeyWidthPercent, numberOfWhites+blackKeyNumber, this.whiteKeysAmount);
+    blackKeyNumber++;
+    numberOfWhitesSinceLastBlack = 0;
+    isFirst = false;
+  }
+}
+
+function newPiano(whiteKeysAmount, blackKeysLayout) {
+  const piano = Object.create(pianoObj);
+
+  // Properties
+  piano.whiteKeysAmount = whiteKeysAmount;
+  piano.blackKeysLayout = blackKeysLayout;
+  piano.blackKeyWidthPercent = 100.0/whiteKeysAmount/1.5;
+  piano.whiteKeyWidthPercent = 100.0/whiteKeysAmount;
+
+  // Utility
+  piano.blackKeysArray = pianoUtilities.arrayFromBlackKeys(blackKeysLayout);
+
+  // DOM
+  piano.HTML = pianoDOM.pianoElementMake.call(piano);
+  buildWhiteKeys.call(piano);
+  buildBlackKeys.call(piano);
+
+  // EventListeners
   pianoEvents.addPianoKeyEventListeners(piano);
 
   return piano;
